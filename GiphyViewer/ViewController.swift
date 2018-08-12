14f4a940f50c2fc9740889b6a9267b7ea68f4fbc
Светлана Лобан <svetlanaloban@svetlanas-macbook-air.local>
2018-08-12 11:43:16 +0300

    Initial Commit

---
+++ GiphyViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GiphyViewer
+//
+//  Created by Светлана Лобан on 8/12/18.
+//  Copyright © 2018 Sviatlana Loban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
